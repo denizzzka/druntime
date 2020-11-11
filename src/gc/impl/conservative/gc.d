453eff45e1977a3ae04d99ec625620eb3e0fa68e
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-11-12 01:15:48 +0700

    (Probably) unused evStart.set() disabled It was call to uninitialized struct

--- src/gc/impl/conservative/gc.d
+++ src/gc/impl/conservative/gc.d
@@ -2792,7 +2792,8 @@ struct Gcx
 
         busyThreads.atomicOp!"+="(1); // main thread is busy
 
-        evStart.set();
+        //FIXME: uninitialized evStart call
+        //evStart.set();
 
         debug(PARALLEL_PRINTF) printf("mark %lld roots\n", cast(ulong)(ptop - pbot));
 
